0c4cbe405957be0944d9ab733ed9c2db1f84edb2
Mohin uddin <Mohinuddin0119@gmail.com>
2024-12-30 11:52:43 +0600

    added grid

--- src/component/countries.jsx
+++ src/component/countries.jsx
@@ -1,24 +1,29 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "./country/country";
+import './countries.css'
 
 const Countries = () => {
-    const [countries,setCountries] = useState([])
+  const [countries, setCountries] = useState([]);
 
-    useEffect(()=>{
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    },[])
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
 
-    return (
-        <div>
-            <h3>Countries: {countries.length}</h3>
-            {
-                countries.map((country) => <Country key={country?.ccn2} country={country}></Country>)
-            }
-        </div>
-    );
+  return (
+    <div>
+      <h3 style={{ color: "tomato", fontWeight: "bold" }}>
+        Countries: {countries.length}
+      </h3>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country key={country?.ccn2} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/component/country/country.jsx
+++ src/component/country/country.jsx
@@ -6,6 +6,9 @@ const Country = ({country}) => {
         <div className='country'>
             <h3>Name: {name?.
 common}</h3>
+        <h5>Capital: {country?.capital}</h5>
+        <h6>Population: {country?.
+population}</h6>
             <img src={flags?.png} alt="" />
         </div>
     );
